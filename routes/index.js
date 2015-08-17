f0004f4f9c65f1a749fa9516505e38dccc1553a5
anspela <elcorreodeansel@gmail.com>
2015-08-17 17:43:43 +0000

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId',quizController.load);
+router.param('commentId',commentController.load);
 
 /* Se definen las rutas y su manejo */
 
@@ -32,6 +33,7 @@ router.delete('/quizes/:quizId(\\d+)',sessionController.loginRequired,quizContro
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/author',function(req, res) {
 	res.render('author',{ title: 'Quiz',errors:[]});
